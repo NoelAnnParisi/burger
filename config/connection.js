a2d078938d3ab24d13799bbc013069d14dd69c4d
Noel Parisi <ycleptnoel@gmail.com>
2017-05-06 14:48:27 -0700

    YAHS

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,7 @@
 const mysql = require('mysql');
-var connection;
-
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: "burgers_db"
-//   });
-// }
+const connection;
 
 connection = mysql.createConnection(process.env.JAWSDB_URL); 
-
-// Make connection.
 connection.connect(function(err) {
 
   if (err) {
@@ -23,23 +9,6 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
-  // connection.create();
 });
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-// });
-
-// connection.connect(err => {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
 
 module.exports = { connection:connection };
