801113d1d8208e102da29ad8e6133487af3c8722
Noel Parisi <ycleptnoel@gmail.com>
2017-05-06 10:39:47 -0700

    db username with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "",
+    user: "ro2hzl4j8p6257np",
+    password: "ckyzvuotumzuetil",
     database: "burgers_db"
 });
 
